0a5f530c0dbffc771f483d53e9179bd3975bf0fa
Tim Atapagra <tim@secureseniorgroup.com>
2021-05-05 14:32:06 -0400

    service to call db methods

---
+++ src/user/service.js
@@ -0,0 +1,55 @@
+class CommonService {
+    /**
+     * @constructor
+     */
+    constructor(model) {
+      this._model = model.createModel()
+    }
+  
+    /**
+     * @prop {MongooseModel} model
+     */
+    get model() {
+      return this._model
+    }
+  
+    /**
+     * @method create
+     * @param {Object} data
+     * @return {Query}
+     */
+    create() {
+      return this.model.create(...arguments)
+    }
+  
+    /**
+     * @method find
+     * @param {Object} [query]
+     * @return {Query}
+     */
+    find() {
+      return this.model.find(...arguments)
+    }
+  
+    /**
+     * @method findOne
+     * @param {Object} [query]
+     * @return {Query}
+     */
+    findOne() {
+      return this.model.findOne(...arguments)
+    }
+  
+     /**
+     * @method findOneAndUpdate
+     * @param {Object} [query]
+     * @return {Query}
+     */
+      findOneAndUpdate(query, update, options = {}) {
+        return this.model.findOneAndUpdate(query, update, { ...options, new: true })
+      }
+  
+  }
+  
+  module.exports = CommonService
+  
